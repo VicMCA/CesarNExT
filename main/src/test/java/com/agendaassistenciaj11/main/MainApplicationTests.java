90f0a5a1c0ebdd6f23e5c356f0da476207289a6b
VictorC (VicMCA) <victor.mcavalcanti@gmail.com>
2021-10-19 17:27:23 -0300

    Projeto de SpringBoot iniciado

---
+++ main/src/test/java/com/agendaassistenciaj11/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendaassistenciaj11.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
