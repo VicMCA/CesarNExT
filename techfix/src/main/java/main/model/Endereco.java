8582b08a6b4e41d3953c0d0fcfe1254275df2cb0
Maria Luiza Reis <mel.reis@gmail.com>
2021-10-25 14:23:15 -0300

    criacao entity Endereco

--- techfix/src/main/java/main/model/Endereco.java
+++ techfix/src/main/java/main/model/Endereco.java
@@ -1,4 +1,46 @@
 package main.model;
 
-public class Endereco {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ENDERECO")
+
+public class Endereco implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "RUA", nullable = false)
+    private String rua;
+
+    @Column(name = "NUMERO", nullable = false)
+    private int numero;
+
+    @Column(name = "COMPLEMENTO", nullable = false)
+    private String complemento;
+
+    @Column(name = "BAIRRO", nullable = false)
+    private String bairro;
+
+    @Column(name = "CIDADE", nullable = false)
+    private String cidade;
+
+    @Column(name = "ESTADO", nullable = false)
+    private String estado;
+
+    @Column(name = "CEP", nullable = false)
+    private String cep;
+
+    @Column(name = "PAIS", nullable = false)
+    private String pais;
+
 }
