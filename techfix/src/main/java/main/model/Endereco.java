9af358107822099edab484031afcb076255668f4
VictorC (VicMCA) <victor.mcavalcanti@gmail.com>
2021-10-25 15:44:17 -0300

    pequena correçao na anotaçao do model Endereco

--- techfix/src/main/java/main/model/Endereco.java
+++ techfix/src/main/java/main/model/Endereco.java
@@ -12,7 +12,6 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Entity
 @Table(name = "ENDERECO")
-
 public class Endereco implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
